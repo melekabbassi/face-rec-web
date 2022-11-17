f57af64c3b2ad0744e3f489b363430e6db0844d2
melekabbassi <abbassimelek@gmail.com>
2022-11-18 00:21:04 +0100

    cors  don't allow getting data yet fix it

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css';
+import React, {useEffect, useState} from 'react';
+
+function App() {
+  const [data, setData] = useState()
+
+  const getApiData = async () => {
+    const response = await fetch('http://127.0.0.1:8000/matches')
+    .then(response => response.json())
+
+    setData(response)
+  }
+
+  useEffect(() => {
+    getApiData()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Face Recognition App</h1>
+      <h2>Matches</h2>
+      <div>
+        { data && data.map((data) => (
+          <div>
+            <p>{data}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
